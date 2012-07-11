4105b99ddb317c98f2b49338484e14cb068ad42e
Shane Riley <shane@shaneriley.com>
2012-07-11 10:45:14 -0400

    Move internal functions to methods on parent plugin object

--- javascripts/jquery.modal.js
+++ javascripts/jquery.modal.js
@@ -6,7 +6,7 @@
   {
     ajax: true, // Whether to load the modal from the href or if it already exists in page
     duration: 500, // Fade in/out speed, in milliseconds
-    position: {}, // An object containing CSS properties for positioning, i.e.: top, left, marginLeft
+    style: {}, // An object containing CSS properties for styling, i.e.: top, left, marginLeft
     modal_layer: "#modal_layer", // id selector for modal layer (full-page overlay)
     modal_container: "<div />", // HTML element used to construct modal
     modal: "#modal" // id selector for modal container
@@ -23,98 +23,80 @@
   #modal_layer is always a div.
 */
 (function($) {
-  $.fn.modal = function(options) {
-    var defaults = {
-      ajax: true,
-      duration: 500,
-      position: {},
+  var modal = {
+    ajax: true,
+    duration: 500,
+    center_modal: true,
+    style: {},
+    selectors: {
       modal_layer: "#modal_layer",
-      modal_container: "<div />",
       modal: "#modal",
-      modal_class: null,
-      ajaxCallback: null,
-      beforeSend: null,
-      beforeShow: null,
-      afterClose: null,
-      bind_type: "live",
-      align_to_trigger: null
-    };
-    var opts = $.extend(defaults, options);
-    opts.left = (parseInt($("body").width()) - opts.width) / 2;
-    var $modal_layer = $(opts.modal_layer),
-        $modal = $(opts.modal),
-        $a;
-    var align_modal = {
-      top: function() { $modal.css("top", $a.offset().top); },
+      context: null
+    },
+    ajaxCallback: null,
+    beforeSend: null,
+    beforeShow: null,
+    afterClose: null,
+    align_to_trigger: null,
+    alignModal: {
+      top: function() { this._parent.$el.css("top", this._parent.$trigger.offset().top); },
       bottom: function() {
-        var top = $a.offset().top - $modal.innerHeight();
-        if (top < 0) { top = opts.position.top || 0; }
+        var top = this._parent.$trigger.offset().top - this._parent.$el.innerHeight();
+        if (top < 0) { top = this._parent.position.top || 0; }
         $modal.css("top", top);
       },
       middle: function() {
-        var top = $a.offset().top - $modal.innerHeight() / 2;
-        if (top < 0) { top = opts.position.top || 0; }
+        var top = this._parent.$trigger.offset().top - this._parent.$el.innerHeight() / 2;
+        if (top < 0) { top = this._parent.position.top || 0; }
         $modal.css("top", top);
       }
-    };
-    return this[opts.bind_type]("click.modal", (opts.ajax) ? create : open);
-    function escBind() {
-      $(document).bind("keyup", function(e) {
-        if (e.keyCode === 27 && $(opts.modal).is(":visible")) {
-          close($(opts.modal), $(opts.modal_layer));
+    },
+    escBind: function() {
+      var m = this;
+      $(document).bind("keyup." + m.name + ".escBind", function(e) {
+        if (e.keyCode === 27 && m.$el.is(":visible")) {
+          m.close();
           $(document).unbind(e);
         }
       });
-    }
-    function configureModal() {
-      var createModalElement = function($e, selector, container) {
-        $e.attributes = {
-          "id": (opts[selector].charAt(0) === "#") ? opts[selector].substring(1) : null,
-          "class": (opts[selector].charAt(0) === ".") ? opts[selector].substring(1) : null
-        }
-        return $(container, $e.attributes).appendTo(document.body);
-      };
-      if (!$modal.length) {
-        $modal = createModalElement($modal, "modal", opts.modal_container);
-      }
-      if (!$modal_layer.length) {
-        $modal_layer = createModalElement($modal_layer, "modal_layer", "<div />");
-      }
-      if (opts.modal_class) { $modal.addClass(opts.modal_class); }
-      return $modal;
-    }
-    function positionModal() {
-      $modal.css($.extend({
+    },
+    createModalElements: function() {
+      var m = this,
+          id = m.selectors.modal.match(/#[a-z0-9\-_]+/i).pop(),
+          klass = m.selectors.modal.match(/\.[a-z0-9\-_]+/i).pop();
+      m.$el = $("<div />", {
+        id: id,
+        "class": klass
+      });
+      id = m.selectors.modal_layer.match(/#[a-z0-9\-_]+/i).pop();
+      klass = m.selectors.modal_layer.match(/\.[a-z0-9\-_]+/i).pop();
+      m.$overlay = $("<div />", {
+        id: id,
+        "class": klass
+      });
+    },
+    position: function() {
+      this.$el.css($.extend({
         left: "50%",
-        marginLeft: -($modal.outerWidth() / 2)
-      }, opts.position));
-    }
-    function open() {
-      $a = $(this);
-      if (typeof opts.beforeSend === "function") {
-        opts.beforeSend($a);
-      }
-      configureModal.apply(this);
-      positionModal();
-      escBind();
-      animateModal();
-      return false;
-    }
-    function animateModal() {
-      if ($modal.innerHeight() > window.innerHeight) {
-        $modal.css({
+        marginLeft: -(this.$el.outerWidth() / 2)
+      }, this.style));
+    },
+    animate: function() {
+      var m = this;
+      if (m.$el.innerHeight() > window.innerHeight) {
+        m.$el.css({
           position: "absolute",
-          top: opts.position.top || window.pageYOffset + 20
+          top: m.style.top || window.pageYOffset + 20
         });
       }
       else {
-        $modal.css({
+        m.$el.css({
           position: "fixed",
-          top: opts.position.top || 20
+          top: m.style.top || 20
         });
       }
-      if (opts.align_to_trigger && opts.align_to_trigger in align_modal) {
-        $modal.css("position", "absolute");
+      if (m.align_to_trigger && m.align_to_trigger in align_modal) {
+        m.$el.css("position", "absolute");
         align_modal[opts.align_to_trigger]();
       }
       if (typeof opts.beforeShow === "function") { opts.beforeShow.apply($a); }
@@ -132,6 +114,56 @@
         return false;
       });
       $modal.trigger("modal_open");
+    },
+    init: function() {
+      var m = this;
+      var clickHandler = function(e) {
+        m.show.call(this, e);
+      };
+
+      !m.$el.length && m.createModalElements();
+      $.each(m, function(o) { $.isPlainObject(o) && (o._parent = m); });
+      if (m.selectors.context) {
+        $(m.selectors.context).on("click." + m.name + ".show", m.$trigger, m.data, clickHandler);
+      }
+      else {
+        m.$trigger.on("click." + m.name + ".show", null, m.data, clickHandler);
+      }
+      m.escBind();
+    }
+  };
+
+  $.fn[modal.name] = function(opts) {
+    var $els = this,
+        method = $.isPlainObject(opts) || !opts ? "" : opts;
+    if (method && modal[method]) {
+      modal[method].apply($els, Array.prototype.slice.call(arguments, 1));
+    }
+    else if (!method) {
+      $els.each(function(i) {
+        var plugin_instance = $.extend(true, { $trigger: $els.eq(i) }, modal, opts);
+        $els.eq(i).data(modal.name, plugin_instance);
+        plugin_instance.init();
+      });
+    }
+    else {
+      $.error('Method ' +  method + ' does not exist on jQuery.' + modal.name);
+    }
+    return $els;
+  };
+
+  $.fn.modal = function(options) {
+    function open() {
+      $a = $(this);
+      if (typeof opts.beforeSend === "function") {
+        opts.beforeSend($a);
+      }
+      configureModal.apply(this);
+      positionModal();
+      animateModal();
+      return false;
+    }
+    function animateModal() {
     }
     function close() {
       for (var i = 0; i < arguments.length; i++) {
